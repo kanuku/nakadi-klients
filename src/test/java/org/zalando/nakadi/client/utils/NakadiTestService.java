838fa9f18af0d339316d8e8d7a47a8894c473f46
Benjamin Friedrich <benjamin.friedrich@zalando.de>
2016-01-27 09:59:59 +0100

    fixed nakadi test service

--- src/test/java/org/zalando/nakadi/client/utils/NakadiTestService.java
+++ src/test/java/org/zalando/nakadi/client/utils/NakadiTestService.java
@@ -12,6 +12,7 @@ import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HeaderMap;
 import io.undertow.util.Headers;
 import io.undertow.util.HttpString;
+import io.undertow.util.StatusCodes;
 import org.apache.commons.io.IOUtils;
 
 import java.util.*;
@@ -59,7 +60,7 @@ public class NakadiTestService {
                         final Handler handler = handlerMap.get(receivedRequestPath);
                         if(handler == null || ! Objects.equals(handler.getRequestMethod(), receivedRequestMethod)) {
                             exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, handler.getResponseContentType());
-                         //   exchange.setResponseCode(HttpStatus.SC_NOT_FOUND);
+                            exchange.setResponseCode(exchange.setResponseCode(StatusCodes.NOT_FOUND);
                             exchange.getResponseSender().send("invalid request: " + receivedRequestPath + " with request method: " + receivedRequestMethod);
                         }
                         else {
