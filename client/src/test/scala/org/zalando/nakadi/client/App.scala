73329c97de734fd252154ae967fb5b8b9da91952
Fernando Benjamin <fernando.benjamin@zalando.de>
2016-04-05 09:16:08 +0200

    LAAS-60 Getting events is done

--- client/src/test/scala/org/zalando/nakadi/client/App.scala
+++ client/src/test/scala/org/zalando/nakadi/client/App.scala
@@ -4,8 +4,8 @@ import org.zalando.nakadi.client.model.JacksonJsonMarshaller
 
 
 object Main extends App with JacksonJsonMarshaller {
-  val host = "nakadi-beta.aruha.zalan.do"
-  val OAuth2Token = () => "4ae2a575-ebd7-40c5-a4de-851b387e00f5"
+  val host = ""
+  val OAuth2Token = () => ""
   val port = 443
   val client = new ClientImpl(Connection.newConnection(host, port, OAuth2Token, true, false),"UTF-8")
 
