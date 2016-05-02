d2beda5ee4ad0bc39343c1af452b0e5615ca80c9
Fernando Benjamin <fernando.benjamin@zalando.de>
2016-05-02 23:07:36 +0200

    techjira:LAAS-60 Adding Java Model!

--- it/src/main/scala/org/zalando/nakadi/client/scala/ClientFactory.scala
+++ it/src/main/scala/org/zalando/nakadi/client/scala/ClientFactory.scala
@@ -2,8 +2,8 @@ package org.zalando.nakadi.client.scala
 object ClientFactory {
   import sys.process._
   import scala.language.postfixOps
-  def host():String = "nakadi-sandbox.aruha-test.zalan.do"
-  def OAuth2Token(): () => String = () =>   "9b77d609-e613-4218-ad9c-fba4d2f0017d"
+  def host():String = "nakadi-sandbox.a-test.a.do"
+  def OAuth2Token(): () => String = () =>   " "
   def getToken():String = OAuth2Token().apply()
   def port():Integer = 443
   def connection():Connection = Connection.newConnection(host, port, OAuth2Token(), true, false)
