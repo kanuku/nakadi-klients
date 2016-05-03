33b590c232f6d0497e9fb37c7f76ece887dd135c
Fernando Benjamin <fernando.benjamin@zalando.de>
2016-05-03 12:32:39 +0200

    techjira:LAAS-60 TESTED AND working as expected! Up tot the Java part!

--- it/src/main/scala/org/zalando/nakadi/client/scala/ClientFactory.scala
+++ it/src/main/scala/org/zalando/nakadi/client/scala/ClientFactory.scala
@@ -3,7 +3,7 @@ object ClientFactory {
   import sys.process._
   import scala.language.postfixOps
   def host():String = "nakadi-sandbox.my-test.fer.do"
-  def OAuth2Token(): () => String = () =>    
+  def OAuth2Token(): () => String = () =>    ""
   def getToken():String = OAuth2Token().apply()
   def port():Integer = 443
   def connection():Connection = Connection.newConnection(host, port, OAuth2Token(), true, false)
