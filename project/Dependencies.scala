788cd7b772808b55de4702ce93e303440048b734
Fernando Benjamin <fernando.benjamin@zalando.de>
2016-05-27 13:10:40 +0200

    techjira:LAAS-60 Fixes #48, #50. Upgrading jackson-scala-module to 2.7.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 object Dependencies {
 
 	val akkaVersion = "2.4.4"
-
+	val jacksonVersion = "2.7.3"
 	val apiDeps = {
 		Seq(
-		"com.fasterxml.jackson.module" %% "jackson-module-scala"   						% "2.6.3",
-		"com.fasterxml.jackson.core"   % "jackson-core"           						% "2.7.0")
+		"com.fasterxml.jackson.module" %% "jackson-module-scala"   						% jacksonVersion,
+		"com.fasterxml.jackson.core"   % "jackson-annotations"     						% jacksonVersion)
 	}
 
 	val clientDeps = {
@@ -15,8 +15,8 @@ object Dependencies {
 			"com.typesafe"                 % "config"                  						% "1.3.0",
 		  "com.google.guava"             % "guava"                   						% "19.0",
 		  "com.typesafe.scala-logging"   %% "scala-logging"          						% "3.1.0",
-		  "com.fasterxml.jackson.core"   % "jackson-core"           						% "2.7.3",
-		  "com.fasterxml.jackson.module" %% "jackson-module-scala"   						% "2.7.3",
+		  "com.fasterxml.jackson.core"   % "jackson-core"           						% jacksonVersion,
+		  "com.fasterxml.jackson.module" %% "jackson-module-scala"   						% jacksonVersion,
 		  "ch.qos.logback"               % "logback-classic"         						% "1.1.3",
 			"com.typesafe.akka"            %% "akka-actor"            	 					% akkaVersion,
 			"com.typesafe.akka"            %% "akka-http-experimental" 						% akkaVersion,
