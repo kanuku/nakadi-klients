7f5a9f6f4cab21a4417f372eb97b240410d8a997
Fernando Benjamin <fernando.benjamin@zalando.de>
2016-06-13 15:26:38 +0200

    techjira:LAAS-60 Fixing the Exceptions by returning the appropriate Result(empty optional) in the java client. Fixes 44

--- client/src/test/scala/org/zalando/nakadi/client/java/JavaClientTest.scala
+++ client/src/test/scala/org/zalando/nakadi/client/java/JavaClientTest.scala
@@ -15,27 +15,29 @@ import java.util.Optional
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.HttpProtocols
 import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.model.ContentTypes
 
-class ClientTest extends WordSpec with Matchers with BeforeAndAfter {
+class JavaClientTest extends WordSpec with Matchers with BeforeAndAfter {
   val connection = mock[Connection]
   val subscriptionHandler = mock[SubscriptionHandler]
   val clientHandler = new JavaClientHandlerImpl(connection, subscriptionHandler)
   val client = new ClientImpl(clientHandler)
-
   val eventTypeName = "EventTypeName"
 
   before {
     reset(connection, subscriptionHandler)
   }
 
-  
+  "Java Client" should {
 
-
-  "Should return Empty Optional when endpoint returns a 404" in {
-    val res = HttpResponse.apply(StatusCodes.NotFound, Nil, HttpEntity.Empty, HttpProtocols.`HTTP/1.1`)
-    val futureResponse = Future.successful(res)
-    when(connection.get(anyString)).thenReturn(futureResponse)
-    val result = client.getEventType(eventTypeName).get
-    result shouldBe Optional.empty()
+    "map a 404 to an Empty Optional" in {
+      val headers = Nil
+      val entity = HttpEntity(ContentTypes.`application/json`, "{}")
+      val response = new HttpResponse(StatusCodes.NotFound, headers, entity, HttpProtocols.`HTTP/1.1`)
+      val futureResponse = Future.successful(response)
+      when(connection.get(anyString)).thenReturn(futureResponse)
+      val result = client.getEventType(eventTypeName).get
+      result shouldBe Optional.empty()
+    }
   }
 }
--- client/src/test/scala/org/zalando/nakadi/client/scala/ScalaClientTest.scala
+++ client/src/test/scala/org/zalando/nakadi/client/scala/ScalaClientTest.scala
@@ -26,38 +26,34 @@ import org.zalando.nakadi.client.utils.Uri
 import org.zalando.nakadi.client.handler.SubscriptionHandlerImpl
 import org.zalando.nakadi.client.handler.SubscriptionHandler
 
-class ClientTest extends WordSpec with Matchers with MockitoSugar with BeforeAndAfter {
+class ScalaClientTest extends WordSpec with Matchers with MockitoSugar with BeforeAndAfter {
   import JacksonJsonMarshaller._
   import Uri._
   private var connection: Connection = mock[Connection]
   private var subscriber: SubscriptionHandler = mock[SubscriptionHandler]
   private val client: Client = new ClientImpl(connection, subscriber)
+  val eventTypeName = "EventTypeName"
   before {
     reset(connection)
   }
-  "ClientImpl " should {
+  "Scala Client " should {
 
-    "return a None when receiving a 404" in {
-      val headers = null
-      val entity = HttpEntity(ContentTypes.`application/json`, "abc")
+    "map a 404 to a None" in {
+      val headers = Nil
+      val entity = HttpEntity(ContentTypes.`application/json`, "{}")
       val response = new HttpResponse(StatusCodes.NotFound, headers, entity, HttpProtocols.`HTTP/1.1`)
       val futureResponse = Future.successful(response)
-      //      when(connection.get(URI_PARTITIONING_STRATEGIES)).thenReturn(futureResponse)
-      //    	val result=Await.result(client.partitionStrategies(),500.seconds)
-      //    	result.isLeft shouldBe true
-      //    	val Left(clientError) = result
-      //    	clientError.status shouldBe None
+      when(connection.get(anyString)).thenReturn(futureResponse)
+      val result =  Await.result(client.getEventType(eventTypeName),5.seconds)
+      result shouldBe Right(None)
     }
 
     "marshall an object when receiving http a 200 with valid payload" in {
-      //TODO : implement
     }
 
     "catch the marshalling exception when receiving a 200 with invalid payload" in {
-      //TODO : implement
     }
     "return a ClientError receiving a redirection (300-399)" in {
-      //TODO : implement
     }
   }
 
