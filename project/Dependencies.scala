d596aa6574f585361d02ae244b45072e6666fb10
Fernando Benjamin <fernando.benjamin@zalando.de>
2016-06-17 13:24:19 +0200

    techjira:LAAS-60 Adding apache commons-lang lib

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,8 @@ object Dependencies {
 		Seq(
 			"org.zalando.stups"                 % "tokens"                  						% "0.9.9",
 			"org.apache.httpcomponents"         % "httpclient"               						% "4.5.2",
-			"org.scalatest"                     %% "scalatest"               						% "2.2.6"
+			"org.scalatest"                     %% "scalatest"               						% "2.2.6",
+			"commons-lang" 											% "commons-lang" 												% "2.6"
 		)
 	}
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -11,3 +11,5 @@ addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "4.0.0")
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.8.2")
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.5")
+
+addSbtPlugin("de.johoop" % "findbugs4sbt" % "1.4.0")
