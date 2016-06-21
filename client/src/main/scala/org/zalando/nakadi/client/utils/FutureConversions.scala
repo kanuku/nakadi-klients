f3c5ba75d1488ae6124f22e754c82896241280a1
Fernando Benjamin <fernando.benjamin@zalando.de>
2016-06-21 10:53:00 +0200

    techjira:LAAS-60 Improvements in the API

--- client/src/main/scala/org/zalando/nakadi/client/utils/FutureConversions.scala
+++ client/src/main/scala/org/zalando/nakadi/client/utils/FutureConversions.scala
@@ -13,10 +13,6 @@ object FutureConversions {
     case Left(error) => throw new RuntimeException(error)
     case Right(t)    => t
   }
-  //  private def extractOption[T >: Null](option: Option[ClientError]): T = option match {
-  //    case Some(v) => throw new RuntimeException(v.msg)
-  //    case None    => null
-  //  }
 
   def fromOption2Optional[T](in: scala.concurrent.Future[Option[T]]): java.util.concurrent.Future[Optional[T]] = {
     new MFuture[Option[T], Optional[T]](in, a => fromOptional2Optional(a))
@@ -31,22 +27,6 @@ object FutureConversions {
           new MFuture[T, Void](in, a => null)
   }
 
-  //  /**
-  //   * Transforms the sequence in the option wrapped in the either right inside a
-  //   * future into a java List wrapped in an Optional wrapped in a Java future.
-  //   * If either left inside the future contains a client error,
-  //   * then a RuntimeException is thrown with the error!
-  //   */
-  //  def fromSeqOfOptionalEither2OptionalList[T](in: scala.concurrent.Future[Either[ClientError, Option[Seq[T]]]]): java.util.concurrent.Future[Optional[java.util.List[T]]] = {
-  //    new MFuture[Either[ClientError, Option[Seq[T]]], Optional[java.util.List[T]]](in, a => fromSeqOfOptionalEither2OptionalList(a))
-  //  }
-  //
-  //  /**
-  //   * Transforms an optional into a Void if it is empty, else RuntimeException is thrown with the error!
-  //   */
-  //  def fromOptional2Future(in: scala.concurrent.Future[Option[ClientError]]): java.util.concurrent.Future[Void] = {
-  //    new MFuture[Option[ClientError], Void](in, a => extractOption(a))
-  //  }
 
   private def fromSequenceToList[T](in: Seq[T]): Optional[java.util.List[T]] = in match {
     case Nil => Optional.empty()
@@ -54,17 +34,6 @@ object FutureConversions {
 
   }
 
-  //  private def fromRightOptionOfEither2Option[R](in: Either[ClientError, Option[R]]): Optional[R] = in match {
-  //    case Left(e)            => throw new RuntimeException(e.msg)
-  //    case Right(Some(value)) => Optional.of(value)
-  //    case Right(None)        => Optional.empty()
-  //  }
-  //  private def fromSeqOfOptionalEither2OptionalList[R](in: Either[ClientError, Option[Seq[R]]]): Optional[java.util.List[R]] = in match {
-  //    case Left(e)          => throw new RuntimeException(e.msg)
-  //    case Right(None)      => Optional.empty()
-  //    case Right(Some(Nil)) => Optional.of(new java.util.ArrayList[R]())
-  //    case Right(Some(seq)) => Optional.of(new java.util.ArrayList[R](seq))
-  //  }
   private def fromOptional2Optional[R](in: Option[R]): Optional[R] = in match {
     case Some(value) => Optional.of(value)
     case None        => Optional.empty()
