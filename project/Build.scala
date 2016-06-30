a28bafa411ec229543cdc2d0fc22db70018b2214
Fernando Benjamin <fernando.benjamin@zalando.de>
2016-06-30 16:44:14 +0200

    techjira:LAAS-60 Development release 2.0.0-pre-alpha.17

--- project/Build.scala
+++ project/Build.scala
@@ -57,7 +57,7 @@ lazy val client = withDefaults(
     project.settings(
         name := projectName,
         organization := "org.zalando.nakadi.client",
-        version := "2.0.0-pre-alpha.16",
+        version := "2.0.0-pre-alpha.17",
         crossPaths := false,
         scalaVersion := "2.11.8",
         publishTo := whereToPublishTo(isSnapshot.value),
