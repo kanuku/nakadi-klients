ce9023d5422159e9096e5ae8497f6e08092d645c
Fernando Benjamin <fernando.benjamin@zalando.de>
2016-07-20 16:14:54 +0200

    techjira:LAAS-60 Feedback:Check parameters to avoid establishing unnecessary communication attempts.

--- client/src/main/java/org/zalando/nakadi/client/java/ClientImpl.java
+++ client/src/main/java/org/zalando/nakadi/client/java/ClientImpl.java
@@ -20,6 +20,8 @@ import org.zalando.nakadi.client.utils.Uri;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 
 public class ClientImpl implements Client {
     private final JavaClientHandler handler;
@@ -51,47 +53,59 @@ public class ClientImpl implements Client {
 
     @Override
     public Future<Void> createEventType(EventType eventType) {
+    	checkNotNull(eventType, "EventType may not be null");
         return handler.post(Uri.URI_EVENT_TYPES(), eventType, eventTypeSerializer);
     }
 
     @Override
     public Future<Optional<EventType>> getEventType(String eventTypeName) {
+    	checkStringIsNotNullOrEmpty(eventTypeName,"eventTypeName may not be null or empty");
         return handler.get(Uri.getEventTypeByName(eventTypeName), eventTypeDeserializer);
     }
 
     @Override
     public Future<Void> updateEventType(String eventTypeName, EventType eventType) {
+    	checkStringIsNotNullOrEmpty(eventTypeName,"eventTypeName may not be null or empty");
+    	checkNotNull(eventType, "EventType may not be null");
        return handler.put(Uri.getEventTypeByName(eventTypeName), eventType, eventTypeSerializer);
     }
 
     @Override
     public Future<Void> deleteEventType(String eventTypeName) {
+    	checkStringIsNotNullOrEmpty(eventTypeName,"eventTypeName may not be null or empty");
         return handler.delete(Uri.getEventTypeByName(eventTypeName));
     }
 
     @Override
     public <T extends Event> Future<Void> publishEvent(String eventTypeName, T event, Serializer<List<T>> serializer) {
+    	checkNotNull(event, "Event may not be null");
         return publishEvents(eventTypeName, Arrays.asList(event), serializer);
     }
 
     @Override
     public <T extends Event> Future<Void> publishEvent(String eventTypeName, T event) {
+    	checkStringIsNotNullOrEmpty(eventTypeName,"eventTypeName may not be null or empty");
+    	checkNotNull(event, "Event may not be null");
         return publishEvents(eventTypeName, Arrays.asList(event));
     }
 
     @Override
     public <T extends Event> Future<Void> publishEvents(String eventTypeName, List<T> events,
             Serializer<List<T>> serializer) {
+    	checkStringIsNotNullOrEmpty(eventTypeName,"eventTypeName may not be null or empty");
+    	checkListIsNotEmptyOrNull(events, "List of events must not be null or empty");
         return handler.post(Uri.getEventStreamingUri(eventTypeName), events, serializer);
     }
 
     @Override
     public <T extends Event> Future<Void> publishEvents(String eventTypeName, List<T> events) {
+    	checkListIsNotEmptyOrNull(events, "List of events must not be null or empty");
         return publishEvents(eventTypeName, events, SerializationUtils.defaultSerializer());
     }
 
     @Override
     public Future<Optional<List<Partition>>> getPartitions(String eventTypeName) {
+    	checkStringIsNotNullOrEmpty(eventTypeName,"eventTypeName may not be null or empty");
         return handler.get(Uri.getPartitions(eventTypeName), seqOfPartitionDeserializer);
     }
 
@@ -114,6 +128,7 @@ public class ClientImpl implements Client {
     @Override
     public <T extends Event> Optional<ClientError> subscribe(String eventTypeName, StreamParameters parameters,
             Listener<T> listener, Deserializer<EventStreamBatch<T>> deserializer) {
+    	checkStringIsNotNullOrEmpty(eventTypeName,"eventTypeName may not be null or empty");
         return handler.subscribe(eventTypeName, Uri.getEventStreamingUri(eventTypeName), parameters, listener,
                 deserializer);
     }
@@ -121,13 +136,28 @@ public class ClientImpl implements Client {
     @Override
     public <T extends Event> Optional<ClientError> subscribe(String eventTypeName, StreamParameters parameters,
             Listener<T> listener, TypeReference<EventStreamBatch<T>> typeRef) {
+    	checkStringIsNotNullOrEmpty(eventTypeName,"eventTypeName may not be null or empty");
+    	checkNotNull(listener, "Listener may not be null");
         return handler.subscribe(eventTypeName, Uri.getEventStreamingUri(eventTypeName), parameters, listener,
                 SerializationUtils.withCustomDeserializer(typeRef));
     }
 
     @Override
     public <T extends Event> void unsubscribe(String eventTypeName, Optional<String> partition, Listener<T> listener) {
+    	checkStringIsNotNullOrEmpty(eventTypeName,"eventTypeName may not be null or empty");
+    	checkNotNull(listener, "Listener may not be null");
         handler.unsubscribe(eventTypeName, partition, listener);
+    }
+    
+    private <T> void checkListIsNotEmptyOrNull(List<T> l, String message){
+    	Preconditions.checkArgument(l!=null&&!l.isEmpty(), message);
+    	
+    }
+    private void checkStringIsNotNullOrEmpty(String in, String message){
+    	Preconditions.checkArgument(!Strings.isNullOrEmpty(in), message);
+    }
+    private <T>void checkNotNull(T obj, String message){
+    	Preconditions.checkNotNull(obj,message);
     }
 
 }
