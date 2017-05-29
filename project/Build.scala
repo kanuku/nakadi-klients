4ee83d928e851e8f50ea3de05e9d8c420467e22d
Fernando Benjamin <kanuku@users.noreply.github.com>
2017-05-29 09:28:11 +0200

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -62,7 +62,7 @@ lazy val client = withDefaults(
     .settings(
         name := projectName,
         organization := "org.zalando.nakadi.client",
-        version := "2.0.4",
+        version := "2.0.5",
         crossPaths := false,
         scalaVersion := "2.11.8",
         findbugsReportType := Some(ReportType.FancyHtml),
